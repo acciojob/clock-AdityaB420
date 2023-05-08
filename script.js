8961a5449a9e2b34c810eec335d7b57dd8428f2b
Aditya Butola <butola.aditya10@gmail.com>
2023-05-08 11:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-//your JS code here. If required.
-var startTime = Date.now();
-      setInterval(function() {
-        var elapsedTime = Date.now() - startTime;
-        var hours = Math.floor(elapsedTime / 3600000);
-        var minutes = Math.floor((elapsedTime % 3600000) / 60000);
-        var seconds = Math.floor((elapsedTime % 60000) / 1000);
-        var timerElement = document.getElementById("timer");
-        timerElement.innerHTML = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds);
-      }, 1000);
-
-      function formatTime(time) {
-        if (time < 10) {
-          return "0" + time;
-        } else {
-          return time;
-        }
-      }
-    
-
-
-
+var datetimeElement = document.getElementById("timer");
+      var now = new Date();
+      datetimeElement.innerHTML = now.toLocaleString();
